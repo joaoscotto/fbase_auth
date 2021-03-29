bdb351ecf625c5bc320482425b75a384f930c923
Scotto <joao.scotto@gmail.com>
2021-03-29 00:10:45 +0000

    initial commit

---
+++ lib/fbauth.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "fbauth/version"
+
+module Fbauth
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/fbauth/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Fbauth
+  VERSION = "0.1.0"
+end
