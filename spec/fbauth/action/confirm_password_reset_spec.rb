f5769c03c6cbc668bb02da3b1c84d1cd3b29b582
Scotto <joao.scotto@gmail.com>
2021-04-04 00:21:21 +0000

    feat(action): add class to confirm password reset

---
+++ spec/fbauth/action/confirm_password_reset_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+RSpec.describe Fbauth::Action::ConfirmPasswordReset do
+  it "path is resetPassword" do
+    expect(described_class::PATH).to eq "resetPassword"
+  end
+
+  describe "#call", :mock_api_host do
+    let(:oob_code) { "test" }
+    let(:new_password) { "test123" }
+    let(:payload) { { oobCode: oob_code, newPassword: new_password }.to_json }
+
+    subject do
+      described_class.new(oob_code: oob_code, new_password: new_password).call
+    end
+
+    it "send request with all data in the payload" do
+      subject
+      assert_requested(:post, @url) { |req| req.body == payload }
+    end
+  end
+end
