31720ca8676a89902b8d0ae2fd05ad7306096965
Scotto <joao.scotto@gmail.com>
2021-04-05 22:48:32 +0000

    feat (request): raise error when the http status is not successful

--- lib/fbase_auth/request.rb
+++ lib/fbase_auth/request.rb
@@ -5,9 +5,13 @@ module FbaseAuth::Request
     url = "#{FbaseAuth.config.host}#{path}?key=#{FbaseAuth.config.api_key}"
     response = HTTP.post url, json: payload
 
-    {
+    wrapper = {
       code: response.code,
       body: JSON.parse(response.to_s)
     }
+
+    return wrapper if [200, 204].include? response.code.to_i
+
+    raise ArgumentError, wrapper
   end
 end
